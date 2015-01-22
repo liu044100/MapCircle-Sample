1585dc01c8838b5f86e18b78f26b7b0bd341a264
ryu-ushin <ryu-ushin@kayac.com>
2015-01-22 09:51:57 +0900

    Initial Commit

---
+++ MapCircleTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapCircleTest
+//
+//  Created by ryu-ushin on 2015/01/22.
+//  Copyright (c) 2015年 rain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MapCircleTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MapCircleTest
+//
+//  Created by ryu-ushin on 2015/01/22.
+//  Copyright (c) 2015年 rain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
